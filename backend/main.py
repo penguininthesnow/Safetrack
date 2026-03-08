1f4654b5c182d0b737a882f93d09d760912bc4d1
Penguininthesnow <chouuu0976286138@gmail.com>
2026-03-09 02:40:57 +0800

    fix

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.database import Base, engine
@@ -5,10 +6,16 @@ from backend.routers import users, inspections
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from backend.database import engine
+from fastapi.staticfiles import StaticFiles
+
 
 print(engine.url) #查找資料庫
 
 app = FastAPI(title="SafeTrack API")
+
+frontend_path = os.path.join(os.path.dirname(__file__), "../frontend")
+
+app.mount("/static", StaticFiles(directory=frontend_path), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,13 +31,16 @@ Base.metadata.create_all(bind=engine)
 app.include_router(users.router)
 app.include_router(inspections.router)
 
-app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
+# @app.get("/")
+# def serve_frontend():
+#     return FileResponse("frontend/index.html")
+# 回傳 index.html
 @app.get("/")
 def serve_frontend():
-    return FileResponse("frontend/index.html")
+    return FileResponse(os.path.join(frontend_path, "index.html"))
 
-import os
+
 
 print("AWS_REGION =", os.getenv("AWS_REGION"))
 print("S3_BUCKET_NAME =", os.getenv("S3_BUCKET_NAME"))

47b39188e27a74ad646170afb3942fa3f1e1a9b1
Penguininthesnow <chouuu0976286138@gmail.com>
2026-03-09 02:58:20 +0800

    fix

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,4 +2,5 @@ const API_BASE =
     window.location.hostname === "127.0.0.1" ||
         window.location.hostname === "localhost"
         ? "http://127.0.0.1:8000"
-        : "https://penguinthesnow.com";
+        : "http://3.86.81.5:8000";
+// : "https://penguinthesnow.com";

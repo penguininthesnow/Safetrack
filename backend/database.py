779a0d2d3d0bc05bb02376a51f8c08446c877773
Penguininthesnow <chouuu0976286138@gmail.com>
2026-03-13 11:04:05 +0800

    add some explanation

--- backend/database.py
+++ backend/database.py
@@ -11,7 +11,11 @@ DB_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DB_URL,
-    pool_pre_ping=True)
+    pool_size=10,
+    max_overflow=20,
+    pool_recycle=1800,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,47 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 from backend.database import Base, engine
 from backend import models
 from backend.routers import users, inspections
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 from backend.database import engine
-# from fastapi.staticfiles import StaticFiles
 
 
 print(engine.url) #查找資料庫
 
 app = FastAPI(title="SafeTrack API")
 
+# frontend path
 frontend_path = os.path.join(os.path.dirname(__file__), "../frontend")
 
+# static files
 app.mount("/static", StaticFiles(directory=frontend_path), name="static")
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://www.penguinthesnow.com",
+        "https://penguinthesnow.com",
+        "http://localhost:8000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# 建立資料表
 # Base是declarative base; Base.metadata是所有模型的資料表資訊集合; _create_all()會根據models建立資料表
 Base.metadata.create_all(bind=engine)
 
+# routers
 app.include_router(users.router)
 app.include_router(inspections.router)
 
-
-# @app.get("/")
-# def serve_frontend():
-#     return FileResponse("frontend/index.html")
-# 回傳 index.html
+# frontend routes
 @app.get("/")
 def serve_frontend():
     return FileResponse(os.path.join(frontend_path, "index.html"))
@@ -45,18 +50,8 @@ def serve_frontend():
 def serve_index():
     return FileResponse(os.path.join(frontend_path, "index.html"))
 
-
+# debug
 print("AWS_REGION =", os.getenv("AWS_REGION"))
 print("S3_BUCKET_NAME =", os.getenv("S3_BUCKET_NAME"))
 print("HAS ACCESS KEY =", bool(os.getenv("AWS_ACCESS_KEY_ID")))
 
-
-
-
-
-
-# 原本 frontend_path 已經定義好了
-# html_files = ["index.html", "inspection.html", "inspection-history.html", "inspection-success.html", "inspection-detail.html"]  # 把你所有 HTML 加進來
-
-# for html_file in html_files:
-#     app.get(f"/{html_file}")(lambda file=html_file: FileResponse(os.path.join(frontend_path, file)))ㄋ

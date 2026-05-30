75bd51052984dbb644e2c3c825672bead3e532a1
Penguininthesnow <chouuu0976286138@gmail.com>
2026-05-30 15:54:21 +0800

    test

--- frontend/js/improvement.js
+++ frontend/js/improvement.js
@@ -3,3 +3,9 @@ const params = new URLSearchParams(window.location.search);
 const inspectionId = params.get("id");
 
 console.log(inspectionId);
+
+document.getElementById("inspectionNumber")
+    .textContent = "INSP-001";
+
+document.getElementById("description")
+    .textContent = "施工現場未配戴安全帽";

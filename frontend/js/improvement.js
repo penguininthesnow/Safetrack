c5fc19ad98a01f42813bff59b263ab71a32ff366
Penguininthesnow <chouuu0976286138@gmail.com>
2026-05-30 16:22:26 +0800

    fix word of script

--- frontend/js/improvement.js
+++ frontend/js/improvement.js
@@ -4,10 +4,13 @@ const params = new URLSearchParams(window.location.search);
 
 const inspectionId = params.get("id");
 
-console.log(inspectionId);
+console.log("inspectionId:", inspectionId);
 
-document.getElementById("inspectionNumber")
-    .textContent = "INSP-001";
+const inspectionNumberEl =
+    document.getElementById("inspectionNumber");
+
+console.log(inspectionNumberEl);
+inspectionNumberEl.textContent = "INSP-001";
 
 document.getElementById("description")
     .textContent = "施工現場未配戴安全帽";

0548d8b65eef842f21cfa62fce0e35acd653a098
Penguininthesnow <chouuu0976286138@gmail.com>
2026-06-03 16:39:57 +0800

    test

--- backend/routers/improvements.py
+++ backend/routers/improvements.py
@@ -1,3 +1,5 @@
+import time
+
 from fastapi import APIRouter, Depends, UploadFile, File, Form
 from sqlalchemy.orm import Session
 from backend.services.s3 import upload_to_s3
@@ -18,6 +20,10 @@ async def create_improvement(
         images: list[UploadFile] = File([]),
         db: Session = Depends(get_db)
 ):
+    start_total = time.time()
+
+    # 1. 建立改善資料
+    t1 = time.time()
     new_improvement = models.Improvement(
         inspection_id=inspection_id,
         improvement_text=improvement_text,
@@ -28,13 +34,30 @@ async def create_improvement(
     db.commit()
     db.refresh(new_improvement)
 
+    print("改善資料建立耗時:", time.time() - t1)
+
+    # 2. 上傳 s3
+    t2 = time.time()
+
     for image in images:
-        image_url = upload_to_s3(image)
-        # 建立圖片資料
-        new_image = models.ImprovementImage(
-            improvement_id=new_improvement.id,
-            image_url=image_url
-        )
-        db.add(new_image)
+        if image and image.filename:
+            try:
+                image_url = upload_to_s3(image)
+                # 建立圖片資料
+                new_image = models.ImprovementImage(
+                    improvement_id=new_improvement.id,
+                    image_url=image_url
+                )
+                db.add(new_image)
+            except Exception as e:
+                print("S3 上傳失敗:", e)
+    
+    print(" S3 上傳耗時:", time.time() - t2)
+
+    # 3. DB commit 圖片
+    t3 = time.time()
     db.commit()
+
+    print("圖片 DB commit 耗時:", time.time() - t3)
+    print("API 總耗時:", time.time() - start_total )
     return new_improvement
